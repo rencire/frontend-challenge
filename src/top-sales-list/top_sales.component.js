975b1b63fffc5b6c9ffaede23082ea7c446bdb4b
Eric Ren <devproren@gmail.com>
2017-05-28 19:10:49 -0700

    Converted back to jsx for kicks

--- src/top-sales-list/top_sales.component.js
+++ src/top-sales-list/top_sales.component.js
@@ -4,33 +4,37 @@ import Styles from './top_sales.scss'
 class TopSalesList extends React.Component  {
     render() {
         return (
-            h('div', null,
-                h('h1', {className: Styles.title}, 'Top Sales Items'),
-                h('ul', {className: Styles.productList},
-                    this.props.products.map((prod) => {
-                        return Product(prod)
-                    })
-                )
-            )
+            <div>
+                <h1 className={Styles.title}>
+                    Top Sales Items
+                    <ul className={Styles.productList}>
+                        {
+                            this.props.products.map((prod) => {
+                                 return <Product key={prod.name} {...prod}/>
+                            })
+                        }
+                    </ul>
+                </h1> 
+            </div>
         )
     }
 };
 
 const Product = (props) => {
     return (
-        h('li', {key: props.name, className: Styles.listItem},
-            h('div', {className: Styles.circle},
-                props.position
-            ),
-            h('div', {className: Styles.productInfo},
-                h('p', {className: Styles.productName},
-                    props.name
-                ),
-                h('p', {className: Styles.productRevenue},
-                    props.revenue
-                )
-            )
-        )
+        <li className={Styles.listItem}>
+            <div className={Styles.circle}>
+                {props.position}
+            </div>
+            <div className={Styles.productInfo}>
+                <p className={Styles.productName}>
+                    {props.name}
+                </p>
+                <p className={Styles.productRevenue}>
+                    {props.revenue}
+                </p>
+            </div>
+        </li>
     )
 }
 
